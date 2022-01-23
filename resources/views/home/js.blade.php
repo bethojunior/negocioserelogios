5aee455643a27268604724ea845208e1a2873dec
betho <hgomescostajr@gmail.com>
2022-01-23 01:13:21 -0300

    add video

---
+++ resources/views/home/js.blade.php
@@ -0,0 +1,5 @@
+<script src="{{ asset('js/libs/jquery.js') }}"></script>
+<script src="{{ asset('js/libs/materialize.min.js') }}"></script>
+<script src="{{ asset('js/libs/sweetalertmin.js') }}"></script>
+<script src="{{ asset('js/utils/SwalCustom.js') }}"></script>
+<script src="{{ asset('js/utils/ElementProperty.js') }}"></script>
---
+++ resources/views/home/nav.blade.php
@@ -0,0 +1,1 @@
+<video id="nav-video" src="{{ asset('assets/images/watch.webm') }}" autoplay="" playsinline="" loop="" preload="auto"  muted="muted"></video>
